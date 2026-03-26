7c67ce4eb45fc6d1d2d1a5a07be47050e9ec6cd6
Ruslan Orazaev <ruslan.orazayevich@gmail.com>
2026-03-26 08:47:39 +0700

    Сделал прорисовку пути

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -14,18 +14,26 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.vector.Path
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
+
 
 const val TILESIZE : Int = 20 //TODO: сделать в пикселях?
 
@@ -48,9 +56,15 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview
+//@Preview
 @Composable
-fun MapView(modifier: Modifier = Modifier) {
+fun MapView(modifier: Modifier = Modifier,
+            path : List<MapPoint> = remember { mutableStateListOf<MapPoint>().apply {
+                    val result = findMyPath()
+                    addAll(result)
+                }
+            }
+) {
     Image(
         painter = painterResource(R.drawable.tsu_map),
         contentDescription = "Карта",
@@ -63,9 +77,44 @@ fun MapView(modifier: Modifier = Modifier) {
                     onTileClicked(row, col)
                 })
             }
+            //создание холста
+            .drawWithContent {
+                //для определения квадратиков для каждого размера телефона
+                val dynamicTileWidth = size.width / 300 // Всего 300 колонок
+                val dynamicTileHeight = size.height / 3600 // Всего 3600 строк
+                //создание непрерывной линии - пути
+                drawContent()
+                val myPath = Path().apply {
+                    val startX = path.first().row * dynamicTileWidth + dynamicTileWidth/2
+                    val startY = path.first().col * dynamicTileHeight + dynamicTileHeight/2
+                    moveTo(startX.toFloat(), startY.toFloat())
+                    path.forEach{
+                        val X = it.row * dynamicTileWidth + dynamicTileWidth/2 //Половинка ячейки для того,
+                        val Y = it.col * dynamicTileHeight + dynamicTileHeight/2 // чтобы не выходить на другие ячейки - пути
+                        lineTo(X.toFloat(), Y.toFloat()) // moveTo,lineTo - требуют float
+                    }
+                }
+                drawPath(
+                    path = myPath,
+                    color = Color.Green,
+                    style = Stroke(width = 8f)
+
+                )
+            }
     )
 }
-
 fun onTileClicked(row:Int, col:Int){
     Log.d("onTileSelectedMessage", "pressed on the " + row + "row and " + col + "column")
 }
+/*
+@Composable
+fun ClearStartAndEndPoint(modifier: Modifier = Modifier,) {
+    Button (
+        onClick = {
+
+        }
+        modifier = Modifier
+    ) {
+        Text (text = "Clear Start Point")
+    }
+}*/
---
+++ app/src/main/java/com/example/myapplication/MapPoint.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+data class MapPoint(val row : Int,
+                    val col : Int
+) {
+
+//    fun first(path : List<MapPoint>) : MapPoint {
+//        return path[0]
+//    }
+}
+
+
+
+fun findMyPath(): List<MapPoint> {
+    val myPath = listOf<MapPoint>(
+        MapPoint(row = 2, col = 3),
+        MapPoint(row = 10, col = 12),
+        MapPoint(row = 100, col = 21),
+        MapPoint(row = 30, col = 300)
+    )
+    return myPath
+}
