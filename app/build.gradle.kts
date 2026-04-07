b553cc31307d33e08a86905dfe2d2d9778c046cf
Ksenia Kuprikova <kuprikova.luda1960@gmail.com>
2026-04-08 00:32:53 +0700

    удалила дублирующиеся зависимости

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.myapplication"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.myapplication"
@@ -37,7 +33,6 @@ android {
     buildFeatures {
         compose = true
     }
-    // Добавил 22:17 23.03.2026
     androidResources {
         noCompress += "png"
     }
@@ -53,8 +48,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.foundation.layout)
-    implementation(libs.androidx.material3)
-    implementation(libs.androidx.ui)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
